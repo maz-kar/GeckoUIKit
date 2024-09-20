e1c2e5da9efe275d255f5d07afaeca299862e158
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-09-20 14:21:53 +0200

    Initial Commit

---
+++ GeckoUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeckoUIKit
+//
+//  Created by Maziar Layeghkar on 20.09.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
