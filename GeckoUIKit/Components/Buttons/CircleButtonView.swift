8cc095ffef6f8b1a7551a805d4a999d9805394a8
Maziar Layeghkar <m.layeghkar@bechstein-digital.com>
2024-09-20 15:44:46 +0200

    circleButtonView was created.

---
+++ GeckoUIKit/Components/Buttons/CircleButtonView.swift
@@ -0,0 +1,54 @@
+//
+//  CircleButtonView.swift
+//  GeckoUIKit
+//
+//  Created by Maziar Layeghkar on 20.09.24.
+//
+
+import UIKit
+
+class CircleButtonView: UIView {
+
+    var imageName: String = "" {
+        didSet {
+            imageView.image = UIImage(systemName: imageName)
+        }
+    }
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+
+    private func setupView() {
+        self.backgroundColor =  UIColor.theme.customBackground
+        self.layer.cornerRadius = 25
+        //self.layer.masksToBounds = true
+        self.layer.shadowColor = UIColor.gray.cgColor
+        self.layer.shadowOpacity = 1.0
+        self.layer.shadowOffset = CGSize(width: 0, height: 2)
+        self.layer.shadowRadius = 10
+        
+        addSubview(imageView)
+        
+        imageView.tintColor = UIColor.theme.customAccent
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 30),
+            imageView.heightAnchor.constraint(equalToConstant: 30)
+        ])
+    }
+}
--- GeckoUIKit/ViewController.swift
+++ GeckoUIKit/ViewController.swift
@@ -8,11 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var CircleButtonView: CircleButtonView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        CircleButtonView.imageName = "chevron.right"
     }
+    
 
 
 }
